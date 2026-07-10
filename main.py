bc15d83adab259dfbb2b9268cc16dc9a8882af4c
jamesgehringer <jamesgehringer@users.noreply.github.com>
2026-07-10 16:17:39 -0500

    Added basic cleaning and line plot for first pass

---
+++ main.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import plotly.express as px
+
+class HistoricalEventFinder:
+
+    def __init__(self, file):
+        self.df = pd.read_csv(file)
+        
+        # Drop rows with missing data in column: 'students5_estimated'
+        self.df = self.df.dropna(subset=['students5_estimated'])
+
+        # Performed 3 aggregations grouped on columns: 'country', 'year'
+        self.df = self.df.groupby(['country', 'year']).\
+            agg(students5_estimated_mean=('students5_estimated', 'mean'),
+                students5_estimated_std=('students5_estimated', 'std'),
+                students5_estimated_sum=('students5_estimated', 'sum')).reset_index()
+
+    def test_print(self):
+        print(self.df)
+
+    def plot_line_chart(self):
+        fig = px.line(
+                self.df, 
+                x="year", 
+                y="students5_estimated_sum", 
+                color="country", # Creates a separate line for each unique value in this column
+                title="Line Graph Separated by Column Values"
+            )
+
+                # Add a vertical shaded block between X-coordinates 2 and 3
+        fig.add_vrect(
+            x0=2000, x1=2010, 
+            fillcolor="red", 
+            opacity=0.5, 
+            layer="below", 
+            line_width=0
+        )
+
+        # 4. Display the chart
+        fig.show()
+
+
+    def main(self):
+        historyObj.test_print()
+        historyObj.plot_line_chart()
+
+if __name__ == "__main__":
+    historyObj = HistoricalEventFinder(r'c:\Users\james\Documents\Repos\cbds-take-home\enrollments.csv')
+    historyObj.main()
--- notes.md
+++ notes.md
@@ -19,8 +19,13 @@ Process
 
 Thoughts
 -Might need to turn the enrollment data into change from previous year
--Can we break it into phasic/tonic portions of the timeseries
+~~-Can we break it into phasic/tonic portions of the timeseries~~
 -Do we need to smooth the data, or will that potentially destroy anything interesting 
+-Relative change vs raw numbers vs normalized to a year
 
 Limitations
 - This is very US centric 
+
+Things I learned
+- Economic down turns may lead to increased enrollment, as people don't have jobs so they go back to school
+- Data set is every 5 years
