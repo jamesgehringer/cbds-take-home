41a1c8cb0885b85370fac75cf6df5e8c668aff84
jamesgehringer <jamesgehringer@users.noreply.github.com>
2026-07-12 12:51:17 -0500

    functionized it a bit more, you can now pass the years through and it all plots dynamically

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class HistoricalEventFinder:
     def test_print(self):
         print(self.df)
 
-    def plot_line_chart(self):
+    def plot_line_chart(self, start_year, end_year):
         fig = px.line(
                 self.df, 
                 x="year", 
@@ -29,7 +29,7 @@ class HistoricalEventFinder:
 
                 # Add a vertical shaded block between X-coordinates 2 and 3
         fig.add_vrect(
-            x0=2000, x1=2010, 
+            x0=start_year, x1=end_year, 
             fillcolor="red", 
             opacity=0.5, 
             layer="below", 
@@ -41,34 +41,39 @@ class HistoricalEventFinder:
 
 
 
-    def plot_change_map(self):
-        change_during_event = pd.DataFrame(columns=["country", "rate_of_change"])
-        for value, subset in self.df.groupby('country'):
-            try:
-                print(subset["country"].iloc[0], subset.query("year == 2010")['students5_estimated_sum'].iloc[0]-subset.query("year == 2000")['students5_estimated_sum'].iloc[0])
-                rate = subset.query("year == 2010")['students5_estimated_sum'].iloc[0]-subset.query("year == 2000")['students5_estimated_sum'].iloc[0]
-                new_row = pd.DataFrame([{'country':subset["country"].iloc[0], 'rate_of_change':rate.astype('float64')}])
-                change_during_event = pd.concat([change_during_event, new_row], ignore_index=True)
-            except IndexError:
-                print(f"{subset["country"].iloc[0]} does not have enough data")
-        
-        change_during_event['rate_of_change'] = pd.to_numeric(change_during_event['rate_of_change'], errors='coerce')
-        
+    def plot_change_map(self, start_year, end_year):
+        change_during_event = self.find_change_over_window(start_year,end_year)
+
         fig2 = px.choropleth(
             change_during_event,
             locations="country",
             locationmode='country names',
             color="rate_of_change",
             color_continuous_scale="Viridis",
-            range_color=(0, 6000000),
+            range_color=(change_during_event["rate_of_change"].min(), change_during_event["rate_of_change"].max()),
         )
         fig2.show()
 
+    def find_change_over_window(self, start_year, end_year):
+        change_during_event = pd.DataFrame(columns=["country", "rate_of_change"])
+        for value, subset in self.df.groupby('country'):
+            try:
+                rate = subset.query("year == @end_year")['students5_estimated_sum'].iloc[0]-subset.query("year == @start_year")['students5_estimated_sum'].iloc[0]
+                new_row = pd.DataFrame([{'country':subset["country"].iloc[0], 'rate_of_change':rate.astype('float64')}])
+                change_during_event = pd.concat([change_during_event, new_row], ignore_index=True)
+            except IndexError:
+                print(f"{subset["country"].iloc[0]} does not have enough data")
+        
+        change_during_event['rate_of_change'] = pd.to_numeric(change_during_event['rate_of_change'], errors='coerce')
+        return change_during_event
+
 
     def main(self):
+        start_year, end_year = 1980, 1995
+        
         historyObj.test_print()
-        historyObj.plot_line_chart()
-        self.plot_change_map()
+        historyObj.plot_line_chart( start_year, end_year)
+        self.plot_change_map( start_year, end_year)
 
 if __name__ == "__main__":
     historyObj = HistoricalEventFinder(r'c:\Users\james\Documents\Repos\cbds-take-home\enrollments.csv')
